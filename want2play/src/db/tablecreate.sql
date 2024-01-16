2ca917dfe79c14a2d90f2b014d1f656646d5ea0c
S3rdna <aaguilar8079@gmail.com>
2024-01-15 17:28:10 -0800

    sql

--- want2play/src/db/tablecreate.sql
+++ want2play/src/db/tablecreate.sql
@@ -1,1 +1,17 @@
-CREATE TABLE  
+CREATE TABLE  Rooms (
+    roomID : TEXT,
+    createDate : DATE,
+    userDataID : INT,
+    PRIMARY KEY(roomName)
+);
+
+CREATE TABLE User_Data (
+    userDataID INT AUTO_INCREMENT,
+    roomID : TEXT,
+    user : TEXT,
+    userData : TEXT,
+    PRIMARY KEY(userDataID,roomID),
+    FOREIGN KEY(roomID) REFERENCES Rooms(roomID) ON DELETE CASCADE
+);
+
+
